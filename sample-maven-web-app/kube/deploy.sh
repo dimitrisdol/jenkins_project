6584de4cd3c146578e9962229a67152bb4198a24
dimitrisdol <jimmdologlou@gmail.com>
2023-03-30 19:05:51 +0300

    Scripts done

--- sample-maven-web-app/kube/deploy.sh
+++ sample-maven-web-app/kube/deploy.sh
@@ -3,4 +3,7 @@
 sleep 60
 cd ..
 cd ..
-./kubectl port-forward service/web-app-service 8088:80
+./kubectl expose deployment web-app --type=NodePort
+PORT=$(kubectl get service web-app --output='jsonpath="{.spec.ports[0].nodePort}"')
+eval echo 192.168.49.2:${PORT}/sample-web-app 
+
