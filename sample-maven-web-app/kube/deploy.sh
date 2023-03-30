0f3e9103cfc524e3b6cf8e5cc3c88c8cbd0152dc
dimitrisdol <jimmdologlou@gmail.com>
2023-03-30 19:09:55 +0300

    Fixed kubectl command

--- sample-maven-web-app/kube/deploy.sh
+++ sample-maven-web-app/kube/deploy.sh
@@ -4,6 +4,6 @@ sleep 60
 cd ..
 cd ..
 ./kubectl expose deployment web-app --type=NodePort
-PORT=$(kubectl get service web-app --output='jsonpath="{.spec.ports[0].nodePort}"')
+PORT=$(./kubectl get service web-app --output='jsonpath="{.spec.ports[0].nodePort}"')
 eval echo 192.168.49.2:${PORT}/sample-web-app 
 
