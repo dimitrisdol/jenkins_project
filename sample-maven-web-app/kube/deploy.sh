2cfbc86b21054246bc1c068ebed3829961a7ba7a
Dimitris Dologlou <57838154+dimitrisdol@users.noreply.github.com>
2023-03-31 21:49:15 +0300

    Update deploy.sh

--- sample-maven-web-app/kube/deploy.sh
+++ sample-maven-web-app/kube/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#Firstly sleep for 1 minute to wait for the Pods to pull the image and be deployed
 sleep 60
+
+#Changing to the directory where kubectl is installed
 cd ..
 cd ..
+
+#Expose the deployment to create a NodePort service
 ./kubectl expose deployment web-app --type=NodePort
+
+#Get that Port and print it with the full IP of the web-app!
 PORT=$(./kubectl get service web-app --output='jsonpath="{.spec.ports[0].nodePort}"')
 eval echo 192.168.49.2:${PORT}/sample-web-app 
 
